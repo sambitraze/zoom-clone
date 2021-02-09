b68936cafaac4122cade78a4bdb9ee35592bca1d
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-02-09 14:01:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+  debug: false
 });
 const { v4: uuidV4 } = require('uuid')
 
